570084a53df4cbb50fcea5a6607e8fd20eac917e
Kishimai <79892199+Kishimai@users.noreply.github.com>
2021-12-12 19:53:19 +0800

    Update

--- Coin Machine Game/Assets/Scripts/ShopSystem.cs
+++ Coin Machine Game/Assets/Scripts/ShopSystem.cs
@@ -5,9 +5,21 @@ using Sirenix.OdinInspector;
 
 public class ShopSystem : MonoBehaviour
 {
-    
+    [BoxGroup("Core Input")]
+    public List<GameObject> placeholder_data = new List<GameObject>();
 
-    public void CalculatePayRate(float BaseUp, float UpgradesHave, float FreeUpgrades){
+    /// <sumamry>
+    /// Calculates the next Total pay rate using the Algorithm
+    /// </summary>
+    public void CalculatePayRate(float BaseUp, float UpgradesHave, float FreeUpgrades){ 
         BaseUp *= 1.15f * UpgradesHave - FreeUpgrades;
+    }
+
+    public void Start() {
+       // Update Current Datas   
+    }
+
+    public void Update_Data(){
+
     }
 }
