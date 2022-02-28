b65d6c1cb38e642aeb938c4609661c13ee645a87
Kishimai <79892199+Kishimai@users.noreply.github.com>
2022-02-28 08:11:55 +0800

    update

--- Coin Machine Game/Assets/Scripts/UI Scripts/ShopSystem.cs
+++ Coin Machine Game/Assets/Scripts/UI Scripts/ShopSystem.cs
@@ -8,97 +8,93 @@ using Sirenix.OdinInspector;
 public class ShopSystem : MonoBehaviour
 {
     [BoxGroup("Input")]
-    public List<GameObject> Coin_list = new List<GameObject>();
+    public List<CoinData> CoinData_List = new List<CoinData>();
     [BoxGroup("Input")]
     public UI_Manager UI;
     [BoxGroup("Input")]
     public CoinGeneration CoinGen;
 
-    GameObject Mostleast; // Most Recent Gameobject that was made
-    
-    
+    [BoxGroup("Main Input")]
+    public CoinData CurrentCoin;
+    [BoxGroup("Main Input")]
+    public GameObject ObjectContent;
+
+    [BoxGroup("Output")]
+    public CoinData UpdatedCoin;
+
+
     public void Start(){
         RestartData();
-        ApplyInfoData();
     }
 
     private void Update() {
-
+        if(UpdatedCoin != CurrentCoin){
+            ApplyInfoData();
+        }
     }
     
     public void ApplyInfoData(){ // Setting infos of Each Coin Data
-        foreach(GameObject _object in Coin_list){
-            CoinData dataInterp = _object.GetComponent<Data_Interp>().data;
-            _object.transform.GetChild(0).GetComponent<Image>().color = dataInterp.CoinColor;
-            _object.transform.GetChild(2).GetComponent<TMP_Text>().text = dataInterp.Name;
-            _object.transform.GetChild(3).GetComponent<TMP_Text>().text = $"${dataInterp.currentValue}";
+        ObjectContent.transform.GetChild(0).GetComponent<Image>().color = CurrentCoin.CoinColor;
+        ObjectContent.transform.GetChild(1).transform.GetChild(0).GetComponent<TMP_Text>().text = CurrentCoin.currentCost.ToString("0");
+        ObjectContent.transform.GetChild(2).GetComponent<TMP_Text>().text = CurrentCoin.Name;
+        ObjectContent.transform.GetChild(3).GetComponent<TMP_Text>().text = $"${CurrentCoin.currentValue} > <color=green>${CurrentCoin.currentValue+CurrentCoin.AddPerLevel}</color>";
 
-            for (int i = 0; i < 10; i++){
-                _object.transform.GetChild(4).transform.GetChild(i).transform.GetChild(0).gameObject.SetActive(false);                  
-            }
-            
-            if(dataInterp.Unlocked == true){ // Means unlocked already for Setted Value
-                UnlockVisualLevel(_object, 0);
-            } else{
-                _object.SetActive(false); // Awaiting for Unlock
-            }
+        for (int i = 0; i < 10; i++){ // Disabiling Visual Levels
+            ObjectContent.transform.GetChild(4).transform.GetChild(i).transform.GetChild(0).gameObject.SetActive(false);                  
         }
+
+        UpdatedCoin = CurrentCoin;
+        UnlockVisualLevel(0);
     }
 
     public void RestartData(){
-        foreach(GameObject _object in Coin_list){
-            var data = _object.GetComponent<Data_Interp>().data;
-            data.currentCost = data.BaseCost;
-            data.CurrentLevel = 0;
-            data.currentValue = data.StartingValue;
+        foreach(CoinData _data in CoinData_List){
+            CurrentCoin.currentCost = CurrentCoin.BaseCost;
+            CurrentCoin.CurrentLevel = 0;
+            CurrentCoin.currentValue = CurrentCoin.StartingValue;
 
-            if(data.name != "Copper"){
-                data.Unlocked = false;
+            if(CurrentCoin.name != "Copper"){
+                CurrentCoin.Unlocked = false;
             } else {
-                data.Unlocked = true;
-                CoinGen.CoinsAvail.Add(_object.GetComponent<Data_Interp>().data);
+                CurrentCoin.Unlocked = true;
             }
 
-            data.levelsForFree = 0;
+            CurrentCoin.levelsForFree = 0;
         }
     }
 
-    public void UnlockVisualLevel(GameObject _object, int UnlockTill){
-        var child = _object.transform.GetChild(4);
+    public void UnlockVisualLevel(int UnlockTill){
+        var child = ObjectContent.transform.GetChild(4);
         child.GetChild(UnlockTill).transform.GetChild(0).gameObject.SetActive(true);
 
-        if(!CoinGen.CoinsAvail.Contains(_object.GetComponent<Data_Interp>().data)){
-            CoinGen.CoinsAvail.Add(_object.GetComponent<Data_Interp>().data);
+        if(!CoinGen.CoinsAvail.Contains(CurrentCoin)){
+            CoinGen.CoinsAvail.Add(CurrentCoin);
         }
     }
 
-    public void DeleteCoin(GameObject _Object){
-        
-    }
-
-    public void UpgradeCoin(GameObject _object){
-        var data = _object.GetComponent<Data_Interp>().data;
-        if(UI._currentCoin >= data.currentCost && data.CurrentLevel != 10){
-            UI._currentCoin -= data.currentCost;
-            data.currentCost = CalculateValue(data.BaseCost, data.CurrentLevel, data.levelsForFree);
-            data.currentValue += data.AddPerLevel;
-            data.CurrentLevel++;
-            UnlockVisualLevel(_object, data.CurrentLevel);
-            _object.transform.GetChild(3).GetComponent<TMP_Text>().text = $"${data.currentValue}";  
+    public void UpgradeCoin(){
+        if(UI._currentCoin >= CurrentCoin.currentCost && CurrentCoin.CurrentLevel != 10){
+            UI._currentCoin -= CurrentCoin.currentCost;
+            CurrentCoin.currentCost = CalculateValue(CurrentCoin.BaseCost, CurrentCoin.CurrentLevel, CurrentCoin.levelsForFree);
+            CurrentCoin.currentValue += CurrentCoin.AddPerLevel;
+            CurrentCoin.CurrentLevel++;
+            ObjectContent.transform.GetChild(3).GetComponent<TMP_Text>().text = $"${CurrentCoin.currentValue} > <color=green>${CurrentCoin.currentValue+CurrentCoin.AddPerLevel}</color>";  
+            ObjectContent.transform.GetChild(1).transform.GetChild(0).GetComponent<TMP_Text>().text = CurrentCoin.currentCost.ToString("0");
 
             // Changed int from 7 to 9, now it unlocks next coin when upgrade reaches maximum
-            if(data.CurrentLevel == 9){
-                foreach(GameObject CoinObject in Coin_list){
-                    if(data.Order + 1 > 6){
-
-                    }
-
-                    if(CoinObject.GetComponent<Data_Interp>().data.Order == data.Order + 1){ // Checking each Coin for the Next Order
-                        CoinObject.SetActive(true); // Set Active so that it is Recognized within the System
-                        UnlockVisualLevel(CoinObject, 0);
+            if(CurrentCoin.CurrentLevel == 9){
+                foreach(CoinData _data in CoinData_List){
+                    if(CurrentCoin.Order != 12){
+                        if(CurrentCoin.Order + 1 == _data.Order){
+                            CurrentCoin = _data;
+                        }
+                    } else{
+                        // Cant Upgrade Maxed out all coins.
                     }
                 }
             }
+
+            UnlockVisualLevel(CurrentCoin.CurrentLevel);
         } else{
             // Cant Upgrade anymore 10 has been reached
         }
